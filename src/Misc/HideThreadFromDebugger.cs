e3b57ce34cac2f713ebe6723eba3aa3c499108b0
Trollicus <40140975+Trollicus@users.noreply.github.com>
2022-06-16 21:56:11 +0300

    Add files via upload

---
+++ src/Misc/HideThreadFromDebugger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using static Anti_Debug_Collection.Structs.HTFD;
+
+namespace Anti_Debug_Collection.Misc;
+
+public class HideThreadFromDebugger
+{
+    public static void HideThreadsFromDebugger()
+    {
+        ProcessThreadCollection currentThreads = Process.GetCurrentProcess().Threads;
+
+        foreach (ProcessThread thread in currentThreads)
+        {
+            IntPtr hProc = OpenThread(ThreadAccess.SET_INFORMATION, false, (uint)thread.Id);
+
+            NtStatus status = NtSetInformationThread(hProc,
+                ThreadInformationClass.ThreadHideFromDebugger, /* Hide Threads from the Debugger */
+                IntPtr.Zero, 0);
+
+            if (status == NtStatus.Success)
+            {
+                Console.WriteLine($"{thread.Id} hidden from debugger.\n");
+            }
+        }
+    }
+}
