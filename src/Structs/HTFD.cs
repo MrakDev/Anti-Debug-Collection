e0282b0c1458d8e16b5aa46efa04d99a2dcff382
Trollicus <40140975+Trollicus@users.noreply.github.com>
2022-06-16 21:56:34 +0300

    Add files via upload

---
+++ src/Structs/HTFD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Anti_Debug_Collection.Structs;
+
+public class HTFD
+{
+    [Flags]
+    public enum NtStatus : uint
+    {
+        // Success
+        Success = 0x00000000
+    }
+    
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle,
+        uint dwThreadId);
+    
+    [Flags]
+    public enum ThreadAccess : int
+    {
+        SET_INFORMATION = (0x0020)
+    }
+    
+    [DllImport("ntdll.dll")]
+    public static extern NtStatus NtSetInformationThread(IntPtr ThreadHandle,
+        ThreadInformationClass threadInformationClass, IntPtr ThreadInformation, ulong ThreadInformationLenght);
+    
+    [Flags]
+    public enum ThreadInformationClass
+    {
+        ThreadHideFromDebugger = 17
+    }
+}
